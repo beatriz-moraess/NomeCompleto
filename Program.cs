8cc534df6167ed85d4096e68029ada4da049078a
Beatriz <beatriz.giovanna@gmail.com>
2025-03-29 19:53:25 -0300

    finaliza programa

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+Console.WriteLine("Escreva seu primeiro nome: ");
+String nome = Console.ReadLine()!;
+
+Console.WriteLine("Escreva seu sobrenome: ");
+String sobrenome = Console.ReadLine()!;
+
+Console.WriteLine($"Nome completo: {nome} {sobrenome}");
+Console.WriteLine($"Nome de catálogo: {sobrenome.ToUpper()}, {nome}");
